a670fd28034c6f4e2a373b2449bcf5f40a613c46
ss2jj <xjontheway@gmail.com>
2014-12-15 17:29:52 +0800

    [basic requirement]the second and third page animation

--- src/com/xujia/preciousgift/MainActivity.java
+++ src/com/xujia/preciousgift/MainActivity.java
@@ -2,17 +2,23 @@ package com.xujia.preciousgift;
 
 import java.util.ArrayList;
 
+
+import java.util.concurrent.CountDownLatch;
+
 import com.xujia.preciousgift.adapter.MyPageViewAdapter;
 import com.xujia.preciousgift.service.MusicPlayService;
 import com.xujia.preciousgift.transformer.*;
 import com.xujia.preciousgift.utils.Utils;
+import com.xujia.preciousgift.view.SurfaceViewTwo;
 
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.app.Activity;
 import android.app.Service;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -21,27 +27,31 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnPageChangeListener{
 private ViewPager myViewPager;
 private ArrayList<View> views = new ArrayList<View>();
 private MyPageViewAdapter adapter;
-private Animation ani_hotball,ani_hotball2,ani_hotball3,ani_textone,ani_music,ani_alpha;
+private Animation ani_hotball,ani_hotball2,ani_hotball3,ani_textone,ani_music,ani_alpha,ani_tianshimove;
+private AnimationDrawable ani_tianshi;
 private ImageButton musicButton;
-private ImageView hotBallView,hotBallView2,hotBallView3,textOneView,textTwoView;
+private ImageView hotBallView,hotBallView2,hotBallView3,textOneView,textTwoView,tianShiView;
 private boolean isMusicOpend = true;
 private static final String ACTION_SERVICE = "com.xujia.preciousgift.musicplayservice";
 private MusicPlayService.MediaPlayerController controller;
 private MyServiceConnection serviceConnection;
-
+private SurfaceViewTwo suerfaceView2;
 
 private Handler handler = new Handler(){
     public void handleMessage(android.os.Message msg) {
@@ -66,6 +76,12 @@ private Handler handler = new Handler(){
             case Utils.START_WENZI2:
             	textTwoView.startAnimation(ani_alpha);
             	textTwoView.setVisibility(View.VISIBLE);
+            	break;
+            case Utils.START_SHOWHEART:
+            	suerfaceView2.shouHeart();
+            	
+            	
+            	break;
             default:
                 break;
         }
@@ -83,12 +99,19 @@ private Handler handler = new Handler(){
 		View view2 = (View)inflater.inflate(R.layout.pageview_two, null);
 		View view3 = (View)inflater.inflate(R.layout.pageview_three, null);
 		
+		Window window = getWindow();
+		int width = window.getWindowManager().getDefaultDisplay().getWidth();
+		int height = window.getWindowManager().getDefaultDisplay().getHeight();	
+		suerfaceView2 =  (SurfaceViewTwo)view2.findViewById(R.id.myServiceView);
+		
+		suerfaceView2.setScreen(width, height);
 		ani_hotball = AnimationUtils.loadAnimation(this, R.anim.hotball_anim);
 		ani_hotball2 = AnimationUtils.loadAnimation(this, R.anim.hotball2_anim);
 		ani_hotball3 = AnimationUtils.loadAnimation(this, R.anim.hotball3_anim);
 		ani_textone = AnimationUtils.loadAnimation(this, R.anim.wenzi_anim);
 		ani_music = AnimationUtils.loadAnimation(this,R.anim.music_anim);
 		ani_alpha = AnimationUtils.loadAnimation(this, R.anim.wenzi2_anim);
+		ani_tianshimove = AnimationUtils.loadAnimation(this, R.anim.tianshi_ani);
 		
 		hotBallView =(ImageView)view1.findViewById(R.id.hotball);
 		hotBallView2 = (ImageView)view1.findViewById(R.id.hotball2);
@@ -96,8 +119,8 @@ private Handler handler = new Handler(){
 		textOneView = (ImageView)view1.findViewById(R.id.wenzi1);
 		textTwoView = (ImageView)view1.findViewById(R.id.wenzi2);
 		musicButton= (ImageButton)findViewById(R.id.music);
-	
-		
+		tianShiView = (ImageView)view3.findViewById(R.id.tianshi);
+		ani_tianshi = (AnimationDrawable) tianShiView.getBackground();
 		musicButton.setAnimation(ani_music);
 
 		views.add(view1);
@@ -108,6 +131,7 @@ private Handler handler = new Handler(){
 		myViewPager.setAdapter(adapter);
 		myViewPager.setPageTransformer(true, new AccordionTransformer());
 		myViewPager.setKeepScreenOn(true);
+		myViewPager.setOnPageChangeListener(this);
 		Intent intent = new Intent();
 		intent.setAction(ACTION_SERVICE);
 		serviceConnection = new MyServiceConnection();
@@ -181,6 +205,37 @@ private Handler handler = new Handler(){
 		public void onServiceConnected(ComponentName name, IBinder service) {
 			// TODO 自动生成的方法存根
 			controller = (MusicPlayService.MediaPlayerController)service;
+		}
+	}
+
+	@Override
+	public void onPageScrollStateChanged(int arg0) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+	@Override
+	public void onPageScrolled(int arg0, float arg1, int arg2) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+	@Override
+	public void onPageSelected(int arg0) {
+		// TODO 自动生成的方法存根
+		Toast.makeText(this, "arg0"+arg0, 1000).show();
+		if(arg0 == 1)	{
+			
+			handler.sendMessageDelayed(handler.obtainMessage(Utils.START_SHOWHEART),1000);
+		}if(arg0 == 2)	{
+			
+			suerfaceView2.clear();
+			com.xujia.preciousgift.utils.BitmapCache.getInstance().clearCache();
+			ani_tianshi.start();
+			tianShiView.startAnimation(ani_tianshimove);
+			
+		}else	{
+			ani_tianshi.stop();
 		}
 	}
 }
